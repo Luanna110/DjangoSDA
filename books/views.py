a8d6e8d18c7439dfc26b59bcd46a4a2994403acb
Anna <anna.chlusewicz@gmail.com>
2022-04-10 12:24:46 +0200

    exercises function-based-views

--- books/views.py
+++ books/views.py
@@ -1,8 +1,10 @@
 from uuid import uuid4
 
+from django.core.exceptions import BadRequest
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def get_hello(request: WSGIRequest) -> HttpResponse:
@@ -25,5 +27,42 @@ def get_uuids_b(request: WSGIRequest) -> JsonResponse:
 # 13. Napisz funkcję przyjmującą argumenty w ściezce (path arguments) i wydrukuj je. Dwa argumenty pierwszy typu int, drugi typu str, trzeci typu slug .
 
 def get_argument_from_path(request: WSGIRequest, x: int, y: str, z: str) -> HttpResponse:
-
     return HttpResponse(f"x= {x}, y= {y}, z= {z}")
+
+# 14. Napisz funkcję przyjmującą argumenty a,b,c jako zapytanie (query arguments <?> [po znaku zapytania]) i wydrukuj je
+
+def get_arguments_from_query(request: WSGIRequest) -> HttpResponse:
+    a = request.GET.get("a")
+    b = request.GET.get("b")
+    c = request.GET.get("c")
+    print(type(a))
+    return HttpResponse(f"a= {a},b= {b},c= {c}")
+
+# 15. Przygotuj funkcję drukująca odpowiedni komunikat dla method HTTP takich jak GET, POST, PUT, DELETE
+
+@csrf_exempt
+def check_http_query_type(request: WSGIRequest)-> HttpResponse:
+    query_type = "Unknown"
+    if request.method == "GET":
+        query_type="to jest GET"
+    elif request.method == "POST":
+        query_type = "to jest POST"
+    elif request.method == "PUT":
+        query_type = "to jest PUT"
+    elif request.method == "DELETE":
+        query_type = "to jest DELETE"
+
+    return HttpResponse(query_type)
+#21 funkcja zwracajaca informacje o headerach
+
+def get_headers(request: WSGIRequest)-> JsonResponse:
+    our_headers = request.headers
+    return JsonResponse({"headers":dict(our_headers)})
+
+@csrf_exempt
+def raise_error_for_fun(request: WSGIRequest)-> HttpResponse:
+    if request.method !="GET":
+        raise BadRequest("method not allow")
+    return HttpResponse("All Good")
+
+
--- library/urls.py
+++ library/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from books.views import get_hello, get_uuids_a, get_uuids_b,get_argument_from_path
+from books.views import get_hello, get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
+    check_http_query_type, get_headers, raise_error_for_fun
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',get_hello),
     path('uuids-a',get_uuids_a),
     path('uuids-b',get_uuids_b),
-    path('path-args/<int:x>/<str:y>/<slug:z>',get_argument_from_path, name="get_from_path")
+    path('path-args/<int:x>/<str:y>/<slug:z>',get_argument_from_path, name="get_from_path"),
+    path('query-args',get_arguments_from_query, name="get_from_query"),
+    path('query-type',check_http_query_type, name="check_query_type"),
+    path('get-header',get_headers, name="get_headers"),
+    path('raise-error',raise_error_for_fun, name="raise_error")
 ]
