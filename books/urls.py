85ca271fe08e567db82bd7eb98a247ea74f501ce
Anna <anna.chlusewicz@gmail.com>
2022-04-24 15:25:53 +0200

    CategoryCreateFormView added

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
     check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListBaseTemplateView, \
-    BookListView, BookDetailsView
+    BookListView, BookDetailsView, CategoryCreateFormView
 
 urlpatterns = [
     path('uuids-a',get_uuids_a),
@@ -15,6 +15,7 @@ urlpatterns = [
     path('author-list',AuthorListBaseView.as_view(), name="author_list"),
     path('category-list',CategoryListBaseTemplateView.as_view(), name="category_list"),
     path('book-list',BookListView.as_view(), name="book_list"),
-    path('book-detail/<int:pk>',BookDetailsView.as_view(), name="book_detail")
+    path('book-detail/<int:pk>',BookDetailsView.as_view(), name="book_detail"),
+    path('category-create',CategoryCreateFormView.as_view(), name="category_create")
 
 ]
