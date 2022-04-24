5e5ef070a1d91537c8fe7ece2da84434f32d96ba
Anna <anna.chlusewicz@gmail.com>
2022-04-24 15:58:31 +0200

    AuthorCreate Added

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
     check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListBaseTemplateView, \
-    BookListView, BookDetailsView, CategoryCreateFormView
+    BookListView, BookDetailsView, CategoryCreateFormView, AuthorCreateView
 
 urlpatterns = [
     path('uuids-a',get_uuids_a),
@@ -16,6 +16,7 @@ urlpatterns = [
     path('category-list',CategoryListBaseTemplateView.as_view(), name="category_list"),
     path('book-list',BookListView.as_view(), name="book_list"),
     path('book-detail/<int:pk>',BookDetailsView.as_view(), name="book_detail"),
-    path('category-create',CategoryCreateFormView.as_view(), name="category_create")
+    path('category-create',CategoryCreateFormView.as_view(), name="category_create"),
+    path('author-create', AuthorCreateView.as_view(), name="author_create")
 
 ]
