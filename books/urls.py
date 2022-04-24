fbf3a6ee8d9e570e6bca6d6d5440d8b6ed8d080e
Anna <anna.chlusewicz@gmail.com>
2022-04-24 12:23:46 +0200

    BookDetailsView added

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
     check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListBaseTemplateView, \
-    BookListView
+    BookListView, BookDetailsView
 
 urlpatterns = [
     path('uuids-a',get_uuids_a),
@@ -14,6 +14,7 @@ urlpatterns = [
     path('raise-error',raise_error_for_fun, name="raise_error"),
     path('author-list',AuthorListBaseView.as_view(), name="author_list"),
     path('category-list',CategoryListBaseTemplateView.as_view(), name="category_list"),
-    path('book-list',BookListView.as_view(), name="book_list")
+    path('book-list',BookListView.as_view(), name="book_list"),
+    path('book-detail/<int:pk>',BookDetailsView.as_view(), name="book_detail")
 
 ]
