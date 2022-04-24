277654561c31aab815326c543b8066f8dc9a0222
Anna <anna.chlusewicz@gmail.com>
2022-04-24 11:41:21 +0200

    CategoryListBaseTemplateView added

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
-    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView
+    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListBaseTemplateView
 
 urlpatterns = [
     path('uuids-a',get_uuids_a),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('query-type',check_http_query_type, name="check_query_type"),
     path('get-header',get_headers, name="get_headers"),
     path('raise-error',raise_error_for_fun, name="raise_error"),
-    path('author-list',AuthorListBaseView.as_view(), name="author_list")
+    path('author-list',AuthorListBaseView.as_view(), name="author_list"),
+    path('category-list',CategoryListBaseTemplateView.as_view(), name="category_list")
 
 ]
