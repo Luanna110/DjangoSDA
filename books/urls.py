ab735775da71252d7580b86a31a4b3f5a96bfd9a
Anna <anna.chlusewicz@gmail.com>
2022-04-24 10:34:31 +0200

    First BookAuthorsView

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
-    check_http_query_type, get_headers, raise_error_for_fun
+    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView
 
 urlpatterns = [
     path('uuids-a',get_uuids_a),
@@ -10,5 +10,7 @@ urlpatterns = [
     path('query-args',get_arguments_from_query, name="get_from_query"),
     path('query-type',check_http_query_type, name="check_query_type"),
     path('get-header',get_headers, name="get_headers"),
-    path('raise-error',raise_error_for_fun, name="raise_error")
+    path('raise-error',raise_error_for_fun, name="raise_error"),
+    path('author-list',AuthorListBaseView.as_view(), name="author_list")
+
 ]
