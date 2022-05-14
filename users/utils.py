eed484522b04c1d9d5c2fe2717c9c1be9a57718f
Anna <anna.chlusewicz@gmail.com>
2022-05-14 13:57:49 +0200

    verify_password_without_salt added

--- users/utils.py
+++ users/utils.py
@@ -3,3 +3,7 @@ import hashlib
 def hash_password_without_salt(password: str) -> str:
     hashed_password = hashlib.sha256(password.encode('ascii')).hexdigest()
     return hashed_password
+
+def verify_password_without_salt(password: str, hashed_password: str) -> bool:
+    input_password = hashlib.sha256(password.encode('ascii')).hexdigest()
+    return input_password==hashed_password
